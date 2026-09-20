85b470d40c2a28866eb68fe34f239c1c123d25da
Matteo Fanelli <mafanelli01@gmail.com>
2026-09-20 17:50:29 -0300

    Termina actividades asignadas

--- components/SiteHeader.jsx
+++ components/SiteHeader.jsx
@@ -1,3 +1,26 @@
+import './SiteHeader.module.css'; /*CSS con los estilos y modificaciones*/
+
+/*configuraciones de telefono*/
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+</head>
+
 export default function SiteHeader() {
-  return <header><strong>UnderGym</strong></header>;
-}
+  return <header>
+    
+    <img src="/logo.png" alt="Logo"/>
+    
+    <strong>
+      UnderGym
+    </strong>
+
+    <h2>
+      Patrocinado por: Aventrix
+    </h2>
+
+    <h3>
+      Stats
+    </h3>
+
+  </header>; 
+} 
